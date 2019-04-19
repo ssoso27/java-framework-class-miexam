68ec8322d792355efc5ccaad872d0aeba66e4889
ssoso27 <sohi8290@naver.com>
2019-04-19 16:27:25 +0900

    [6] Datasource

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,18 +1,41 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String classname;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
 
     @Bean
-    public ConnectionMaker getConnectionMaker() {
-        return new JejuConnectionMaker();
+    public UserDao userDao() {
+        return new UserDao(dataSource());
     }
 
     @Bean
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        try {
+            dataSource.setDriverClass((Class<? extends Driver>) Class.forName(classname));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException();
+        }
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
     }
 }
