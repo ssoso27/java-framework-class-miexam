216aeae94ec5c66558aa439298ca33e97ffd70c5
ssoso27 <sohi8290@naver.com>
2019-04-19 16:22:09 +0900

    [5] change to Spring

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,11 +1,17 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public ConnectionMaker getConnectionMaker() {
         return new JejuConnectionMaker();
     }
 
+    @Bean
     public UserDao getUserDao() {
         return new UserDao(getConnectionMaker());
     }
